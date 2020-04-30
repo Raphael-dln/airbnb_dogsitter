bf93de4ae32ef9eae4c5eba51cfd4a2102696f86
Raphael-dln <delannay.raphael@gmail.com>
2020-04-30 22:43:42 +0200

    comments & readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,25 @@
 require 'faker'
-
+# Création des villes
 10.times do
   city = City.create!(city_name: Faker::Movies::HarryPotter.location)
 end
 
+# Création des chiens
 15.times do
   dog = Dog.create!(doggy_name: Faker::Creature::Dog.name, city: City.all.sample)
 end
 
+# Création des promeneurs
 15.times do
   dogsitter = Dogsitter.create!(full_name: Faker::Movies::HarryPotter.character, city: City.all.sample)
 end
 
+# Création des promenades
 10.times do
   stroll = Stroll.create!(date: Faker::Time.forward(days: 150, period: :day), dogsitter: Dogsitter.all.sample, city: City.all.sample)
 end
 
+# Création des groupes de chiens pour faire le lien entre les tables strolls / dogs
 100.times do
   outing = Outing.create!(dog: Dog.all.sample, stroll: Stroll.all.sample, city: City.all.sample)
 end
