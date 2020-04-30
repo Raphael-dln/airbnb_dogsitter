0801c25cd7c59b49e6e8318dfbc9791eb57b9b1b
Raphael-dln <delannay.raphael@gmail.com>
2020-04-30 23:37:11 +0200

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
 require 'faker'
-<<<<<<< HEAD
 
-City.destroy_all
-Dog.destroy_all
-Dogsitter.destroy_all
-Outing.destroy_all
-Stroll.destroy_all
 
 5.times do
   city = City.create!(city_name: Faker::Movies::HarryPotter.location)
@@ -24,26 +18,6 @@ end
   stroll = Stroll.create(date: Faker::Time.forward(days: 150, period: :day), city: City.all.sample)
   stroll.dogsitter = Dogsitter.where("city_id like ?", "%#{stroll.city_id}%").sample
   stroll.save
-=======
-# Création des villes
-10.times do
-  city = City.create!(city_name: Faker::Movies::HarryPotter.location)
-end
-
-# Création des chiens
-15.times do
-  dog = Dog.create!(doggy_name: Faker::Creature::Dog.name, city: City.all.sample)
-end
-
-# Création des promeneurs
-15.times do
-  dogsitter = Dogsitter.create!(full_name: Faker::Movies::HarryPotter.character, city: City.all.sample)
-end
-
-# Création des promenades
-10.times do
-  stroll = Stroll.create!(date: Faker::Time.forward(days: 150, period: :day), dogsitter: Dogsitter.all.sample, city: City.all.sample)
->>>>>>> bf93de4ae32ef9eae4c5eba51cfd4a2102696f86
 end
 
 # Création des groupes de chiens pour faire le lien entre les tables strolls / dogs
