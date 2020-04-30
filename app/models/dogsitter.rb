1d136941a063d79a1f759833aca65372a9af37e0
agnesarth <agnesarth@hotmail.fr>
2020-04-30 23:19:03 +0200

    FIniiiii

--- app/models/dogsitter.rb
+++ app/models/dogsitter.rb
@@ -1,5 +1,6 @@
 class Dogsitter < ApplicationRecord
   belongs_to :city
   has_many :strolls, dependent: :destroy
-  has_many :dogs, through: :strolls
+  has_many :outings, through: :strolls
+  has_many :dogs, through: :outings
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,37 @@
 require 'faker'
 
-10.times do
+City.destroy_all
+Dog.destroy_all
+Dogsitter.destroy_all
+Outing.destroy_all
+Stroll.destroy_all
+
+5.times do
   city = City.create!(city_name: Faker::Movies::HarryPotter.location)
 end
 
-15.times do
+
+120.times do
   dog = Dog.create!(doggy_name: Faker::Creature::Dog.name, city: City.all.sample)
 end
 
-15.times do
+50.times do
   dogsitter = Dogsitter.create!(full_name: Faker::Movies::HarryPotter.character, city: City.all.sample)
 end
 
-10.times do
-  stroll = Stroll.create!(date: Faker::Time.forward(days: 150, period: :day), dogsitter: Dogsitter.all.sample, city: City.all.sample)
+100.times do
+  stroll = Stroll.create(date: Faker::Time.forward(days: 150, period: :day), city: City.all.sample)
+  stroll.dogsitter = Dogsitter.where("city_id like ?", "%#{stroll.city_id}%").sample
+  stroll.save
 end
 
 100.times do
-  outing = Outing.create!(dog: Dog.all.sample, stroll: Stroll.all.sample, city: City.all.sample)
+  outing = Outing.create(city: City.all.sample)
+  outing.stroll = Stroll.where("city_id like ?", "%#{outing.city_id}%").sample
+  outing.dog = Dog.where("city_id like ?", "%#{outing.city_id}%").sample
+  outing.save
 end
+
 
 
 
